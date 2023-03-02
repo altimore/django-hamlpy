138be47bbadb38cf4f609b6aeef834e39f8156cf
Norbert Kwizera <norkans7@gmail.com>
2023-03-02 17:47:45 +0200

    Add apps submodule

--- hamlpy/__init__.py
+++ hamlpy/__init__.py
@@ -1,14 +1,1 @@
-from django.apps import AppConfig
-
 HAML_EXTENSIONS = ("haml", "hamlpy")
-
-
-class Config(AppConfig):
-    name = "hamlpy"
-
-    def ready(self):
-        # patch Django's templatize method
-        from .template import templatize  # noqa
-
-
-default_app_config = "hamlpy.Config"
